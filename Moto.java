fa6ca3f9ccf3f1f9a69e1229da58c2b6f865f8cf
SumrakkNote <SumrakkNote>
2024-01-30 12:34:51 +0300

    second

--- Moto.java
+++
@@ -1,7 +0,0 @@
-package com.sumrakk;
-
-public class Moto extends Vehicle{
-    public Moto(int yearRelease, float speed, String color, byte[] coordinate) {
-        super(yearRelease, speed, color, coordinate);
-    }
-}
