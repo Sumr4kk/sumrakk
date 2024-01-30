54dc303eaa998921fbc5f3122ed571af997827bc
SumrakkNote <SumrakkNote>
2024-01-30 12:44:12 +0300

    third

--- Lesson.java
+++ Lesson.java
@@ -5,5 +5,6 @@ public class Lesson {
         Truck truck = new Truck(50.5f, new byte[] {1, 2, 3}, false);
         truck.setValues(2024, 360.5f, "Rainbow", new byte[] {100, 0, 100}, true);
         System.out.println(truck.getValues());
+        // Привет
     }
 }
